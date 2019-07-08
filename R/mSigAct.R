227566e73440a1a11a88db68b41b79905f5f2387
steverozen <steve@steverozen.net>
2019-07-08 13:05:54 +0800

    First function in the package is defined.

--- R/mSigAct.R
+++ R/mSigAct.R
@@ -1,20 +1,23 @@
-#' Negative binomial log likelihood that an observed spectrum was generated from an observed vector of expected counts.
+#' Negative binomial log likelihood that an observed spectrum was generated from a vector of expected counts.
 #'  
 #' @param spectrum An observed spectrum (a numeric vector)
 #' 
 #' @param expected.counts A vector of expected mutation counts, one
 #' expected count for each mutation type. We want to know the 
 #' likelihood that this model generated the observed 
-#' spectrum, assuming each mutational generates counts according to
+#' spectrum, assuming each mutational types generates counts according to
 #' a negative binomial distribution with 
-#' the given \code{expected.counts} and dispersion parameter
-#' \code{nbinom.size}. See \code{\link[stats]{nbinom}}.
+#' the given \code{expected.counts} (argument \code{m}
+#' to \code{\link[stats]{dnbinom}}) and dispersion parameter
+#' \code{nbinom.size}.
 #' 
 #' @param nbinom.size The \code{size} parameter that
-#' governs dispersion. See \code{\link[stats]{nbinom}}.
+#' governs dispersion. See \code{\link[stats]{dnbinom}}.
 #' Smaller values correspond to larger dispersion.
 #'
 #' @return  \code{log(likelihood(spectrum | expected.counts))}
+#' 
+#' @importFrom stats dnbinom
 #'
 #' @export
 
