466ca3276e0283d58801cbe65adfd8c5313fccc1
steverozen <steve@steverozen.net>
2019-07-23 22:46:09 +0200

    Continuing to make this a package; now has code to esimate a background signature and its parameters and the arguments for the objective function for finding a signature have been sketched out.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Specifications of background signatures
+#'
+#
+#'
+#' @format A list with the elements MORE MORE MORE
+#' 
+#' @name background.info
+#' 
+#' @examples 
+#' # MORE MORE MORE
+#' 
+"HepG2.background.info"
---
+++ data-raw/Estimate.HepG2.background.signature.R
@@ -0,0 +1,3 @@
+library(ICAMS) # Needed temporarily pending ICAMS update
+MakeHepG2BackgroundPart1()
+HepG2.background.info <- MakeHepG2BackgroundPart2(maxeval = 50000)
