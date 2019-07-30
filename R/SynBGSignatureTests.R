26c31defa6f458892426222b9dbe307eaec5a292
Steven G. Rozen <steve@steverozen.net>
2019-07-31 01:02:38 +0800

    Almost finished with generating background data; renamed file with tests.

--- R/SynBGSignatureTests.R
+++ R/SynBGSignatureTests.R
@@ -18,7 +18,7 @@ DistancesToSPSigs <- function() {
 MakeTest <- function(rep.num, 
                      bg.sig.info,
                      bg.contribution,
-                     ref.sig) {
+                     target.sig.name) {
   # To figure out the non-background, do we want
   # to have different background levels based
   # on distribution of intensities, and then
@@ -32,52 +32,50 @@ MakeTest <- function(rep.num,
   # with mean bg.sig.info$count.nbinom.mu and
   # size parameters bg.sig.info$count.nbinom.size
   
-  mu <- bg.sig.info$count.nbinom.mu
+  bg.mu   <- bg.sig.info$count.nbinom.mu
   size <- bg.sig.info$count.nbinom.size
   
-  bg.count <- stats::rnbinom(1, mu = mu, size = size)
+  bg.count <- stats::rnbinom(1, mu = bg.mu, size = size)
   
   # Draw the "target" signature contribution from
   # the negative binomial distribution with mean
   # 1/(1 - bg.contribution) * bg.sig.info$count.nbinom.mu
   # and size bg.sig.info$count.nbinom.size
   
-  target.count <-
-    stats::rnbinom(1, mu = 1/(1 - bg.contribution) * mu, size = size)
+  target.mu <- bg.mu * (1 - bg.contribution)/bg.contribution
   
-  spectrum <- bg.count * bg.sig.info$background.sig
-              + target.count * ref.sig
+  target.count <- stats::rnbinom(1, mu = target.mu, size = size)
   
-  list(bg.count = bg.count, target.count = target.count, spectrum = spectrum)
+  ref.sig <- mSigAct::sp.sigs[ , target.sig.name, drop = FALSE]
+  
+  spectrum <-
+    bg.count * bg.sig.info$background.sig + target.count * ref.sig
+  
+  c(target.sig.name = target.sig.name,
+       bg.contribution = bg.contribution,
+       bg.mu           = bg.mu,
+       bg.count        = bg.count, 
+       target.mu       = target.mu,
+       target.count    = target.count,
+       spectrum        = spectrum)
   
 }
 
-
-TestOneSigAndContribution <- function(
-  bg.sig.info, bg.contribution, ref.sig, num.replicates) {
-  
-  mc.cores.to.use <-
-    ifelse(Sys.info()["sysname"] == "Windows", 1, num.replicates)
-
-  mc.output <-
-    parallel::mclapply(1:num.replicates,
-             FUN = MakeTest,
-             bg.sig.info     = bg.sig.info,
-             bg.contribution = bg.contribution,
-             ref.sig         = ref.sig,
-             mc.cores = mc.cores.to.use)
-  
-  # Not done, need to get the important part of 
-  # mc.output and check for errors
-  return(mc.output)
+TestMakeTest <- function() {
+  r1 <- MakeTest(1, HepG2.background.info, 0.1, "SBS6")
+  r2 <- MakeTest(2, HepG2.background.info, 0.1, "SBS6")
+  rx <- rbind(r1, r2)
+  return(rx)
 }
+
+# Note: https://www.r-bloggers.com/populating-data-frame-cells-with-more-than-one-value/
 
 # Set up a grid of HepG2 mixture of 0.1, 0.5, 0.9
 # Do 10 replicates at each mixture, draw
 # the number HepG2 mutations from the values in HepG2.background.info,
 # then back into the total number of mutations.
 
-AllSynTests <- function() {
+MakeAllHepGSynTests <- function() {
   sig.names.to.test <- c("SBS40", "SBS4", "SBS58", "SBS6")
   contribution <- c(0.1, 0.5, 0.9)
   dist <- round(DistancesToSPSigs(), digits = 1)
@@ -89,28 +87,18 @@ AllSynTests <- function() {
   set.seed(441441)
   # check that this works -- otherwise clusterSetRNGStream
   
-  output <- 
-    array(data = NA, 
-          dim = c(num.replicates,
-                  length(contribution),
-                  length(sig.names)),
-          dimnames = list(
-            replicate = paste0("replicate",
-                               formatC(1:num.replicates,
-                                       format = "d",
-                                       width = 2,
-                                       flag = "0")),
-            contribution = as.character(contribution),
-            signature    = sig.names))
-  
-  for (i in 1:length(sigs.to.test)) {
+  output <- numeric(102)
+
+  for (i in 1:length(sig.names.to.test)) {
     for (cont in contribution) {
-      output[sig.names[i], cont, ] <-
-        TestOneSigAndContribution(
-          bg.sig.info     = mSigAct::HepG2.background.info, 
-          bg.contribution = cont,
-          target.sig      = sigs.to.test[i],
-          num.replicates  = num.replicates)
+      for (replicate in 1:num.replicates) {
+        next.row <-
+          MakeTest(rep.num         = replicate, 
+                   bg.sig.info     = HepG2.background.info,
+                   bg.contribution = cont,
+                   target.sig.name = sig.names.to.test[i])
+        output <- rbind(output, next.row)
+      }
     }
   }
   return(output)
