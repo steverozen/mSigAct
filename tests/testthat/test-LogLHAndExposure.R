5f1e5e9610f9599adcfe349be7eff4f2cba8f837
steverozen <steve@steverozen.net>
2019-10-23 14:50:53 +0800

    added new tests for one.lh.and.exp

---
+++ tests/testthat/test-LogLHAndExposure.R
@@ -0,0 +1,61 @@
+context("LogLHAndExposure")
+
+PrepOneSynSpectrum <- function(sig.counts,
+                               input.sigs = PCAWG7::signature$genome$SBS96) {
+  sig.names <- names(sig.counts)
+  
+  if (sum(sig.names %in% colnames(input.sigs)) == 0) {
+    stop("sig.names not all in input.sigs")
+  }
+  
+  some.sigs  <- input.sigs[ , sig.names, drop = FALSE]
+  ref.genome <- attr(some.sigs, "ref.genome", exact = TRUE)
+  region     <- attr(some.sigs, "region", exact = TRUE)
+  if (is.null(region)) {
+    message("Null region, why?")
+    region <- "genome"
+  }
+  
+  spectrum <- round(some.sigs %*% sig.counts)
+  spectrum <-
+    ICAMS::as.catalog(
+      object       = spectrum, 
+      ref.genome   = ref.genome,
+      region       = region,
+      catalog.type = "counts")
+  
+  return(list(sigs = some.sigs, spec = spectrum))
+}
+
+TestOneLLHetc <- function(sig.counts,
+                          input.sigs = PCAWG7::signature$genome$SBS96,
+                          eval_f = obj.fun.nbinom.maxlh,
+                          trace = 0) {
+  
+  test.data <- PrepOneSynSpectrum(sig.counts = sig.counts,
+                                  input.sigs = input.sigs)
+  
+  
+  m.opts <- DefaultManyOpts()
+  m.opts$trace <- trace
+  
+  retval <- one.lh.and.exp(
+    spect  = test.data$spec,
+    sigs   = test.data$sigs, 
+    m.opts = m.opts,
+    eval_f = eval_f)
+ 
+  return(c(retval, list(m.opts = m.opts)))
+}
+
+
+test_that("LogLHAndExposure 1", {
+  input <- c(SBS1 = 1000, SBS22 = 2000)
+  retval <- TestOneLLHetc(sig.counts = input)
+  testthat::expect_equal(retval$loglh, -222.7188, tolerance = 1e-5)
+  testthat::expect_equal(retval$exposure,
+                         c(SBS1 = 999.2467, SBS22 = 1998.7533))
+  testthat::expect_equal(retval$m.opts$nbinom.size, 5)
+  testthat::expect_equal(retval$m.opts$global.opts$algorithm,
+                         "NLOPT_GN_DIRECT")
+})
