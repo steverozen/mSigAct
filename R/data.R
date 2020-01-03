fedc21ff2f0997497222d2103d2e199c55b643a9
steverozen <steve@steverozen.net>
2020-01-03 18:37:00 +0800

    added nitrosamine example spectra

--- R/data.R
+++ R/data.R
@@ -160,4 +160,12 @@
 #'
 "BG.MEG.kucab.bg.test"
 
-
+#' Example nitrosamine data for background subtraction
+#'
+#' @format A list of spectra catalogs for nitrosamines.
+#'  The names of the catalogs are self-explanatory.
+#'  Each catalog has 2 spectra from each of four different
+#'  nitrosamines, "NDEA", "NDMA", "NPIP", "NPYR". The
+#'  samples names also should be self-explanatory.
+#'
+"nitrosamine.examples"
