0669539ef8aa175b8c5392c807ec1da640a401fb
steverozen <steve@steverozen.net>
2020-03-03 16:23:48 -0500

    clean up data-raw

---
+++ data-raw/background.signature.spectra/PrepareBackgroundInfoPackageData.R
@@ -0,0 +1,6 @@
+# Sketch of how to create information on background data
+# 
+
+mSigAct:::MakeMCF10HepG2BackgroundVar() # Creates the package data for spectra for backgrounds
+
+mSigAct:::SaveHepG2andMCF10ABackgroundInfo() # Creates the package data for the background.info 
