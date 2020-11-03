d01d15e859b3cca42ca7b92a1bb958bdeb9b9386
Steven G. Rozen <steve@steverozen.net>
2020-11-03 17:38:25 +0800

    sketch of profiling code in tests/testthat

--- tests/testthat/short-SparseAssignActivity1.R
+++ tests/testthat/short-SparseAssignActivity1.R
@@ -1,24 +1,28 @@
-  set.seed(101010, kind = "L'Ecuyer-CMRG")
+set.seed(101010, kind = "L'Ecuyer-CMRG")
 
 
-  some.sigs  <-
-    PCAWG7::signature$genome$SBS96[ , 1:2, drop = FALSE]
-  ref.genome <- attr(some.sigs, "ref.genome", exact = TRUE)
-  region     <- attr(some.sigs, "region", exact = TRUE)
-  if (is.null(region)) {
-    message("Null region, why?")
-    region <- "genome"
-  }
+some.sigs  <-
+  PCAWG7::signature$genome$SBS96[ , 1:2, drop = FALSE]
+ref.genome <- attr(some.sigs, "ref.genome", exact = TRUE)
+region     <- attr(some.sigs, "region", exact = TRUE)
+if (is.null(region)) {
+  message("Null region, why?")
+  region <- "genome"
+}
 
-  spect <- round(some.sigs %*% c(100,100))
-  spect <-
-    ICAMS::as.catalog(
-      spect,
-      ref.genome   = ref.genome,
-      region       = region,
-      catalog.type = "counts")
+spect <- round(some.sigs %*% c(100,100))
+spect <-
+  ICAMS::as.catalog(
+    spect,
+    ref.genome   = ref.genome,
+    region       = region,
+    catalog.type = "counts")
 
-  m.opts <- DefaultManyOpts()
+m.opts <- DefaultManyOpts()
+
+library(profvis)
+
+ww <- profvis(
 
   SA.out <- SparseAssignActivity1(spect       = spect,
                                   sigs         = some.sigs,
@@ -27,5 +31,8 @@
                                   max.level            = 1,
                                   max.mc.cores         = 1,
                                   num.parallel.samples = 1
-  )
+  ) )
+
+htmlwidgets::saveWidget(ww, "0profile.ww.html")
+
 
