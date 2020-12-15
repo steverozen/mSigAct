30248bbbb069def8cb54d57ffd3dfa71f7f384f2
Steven G. Rozen <steve@steverozen.net>
2020-12-15 18:04:58 +0800

    added error checking to script

--- data-raw/get.spect.R
+++ data-raw/get.spect.R
@@ -4,12 +4,17 @@ args <- commandArgs(trailingOnly = TRUE)
 mutation.type <- args[1]
 id <- args[2]
 message(mutation.type)
+
+if (!mutation.type %in% c("DBS78"))
+
 message(id)
 
 # "Bladder-TCC::SP1003"
 
+ss <- PCAWG7::spectra$PCAWG[[mutation.type]]
+if (!id %in% colnames(ss)) stop(id, " not found")
 
-cc <- PCAWG7::spectra$PCAWG[[mutation.type]][ , id, drop = FALSE]
+cc <- ss[ , id, drop = FALSE]
 
 id <- gsub("::", "_", id)
 ff <- paste0(mutation.type, "_", id, ".csv")
