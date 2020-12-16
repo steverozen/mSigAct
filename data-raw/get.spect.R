abee842e8ffde2948e4b40cbaf473eb4b07b801e
Steven G. Rozen <steve@steverozen.net>
2020-12-16 23:02:51 +0800

    upated get.spect.R and generated some test data

--- data-raw/get.spect.R
+++ data-raw/get.spect.R
@@ -1,11 +1,23 @@
 #!Rscript
+
+# Script to create a 1-sample spectrum catalog from the PCAWG7 
+# data package, from the PCAWG platinum data.
+
 args <- commandArgs(trailingOnly = TRUE)
+
+if(length(args) != 2) {
+  stop("Usage: get.spect.R <mutation type> <sample id>")
+}
 
 mutation.type <- args[1]
 id <- args[2]
 message(mutation.type)
 
-if (!mutation.type %in% c("DBS78"))
+known.sig.types <-c("DBS78","ID", "SBS96", "SBS192", "SBS1536")
+if (!mutation.type %in% known.sig.types) {
+  stop("First argument must be one of ",
+          paste(known.sig.types, collapse = ", "))
+}
 
 message(id)
 
