05a459b185732af630b9eb442bf20f6d232ae26e
Steven G. Rozen <steve@steverozen.net>
2020-12-18 16:52:06 +0800

    updated get.spect.R, reorganized test data

--- data-raw/get.spect.R
+++ data-raw/get.spect.R
@@ -5,12 +5,12 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 
-if(length(args) != 2) {
-  stop("Usage: get.spect.R <mutation type> <sample id>")
+if(length(args) < 2) {
+  stop("Usage: get.spect.R <mutation type> <sample id 1> [<sample id 2> ...]")
 }
 
 mutation.type <- args[1]
-id <- args[2]
+id <- args[2:length(args)]
 message(mutation.type)
 
 known.sig.types <-c("DBS78","ID", "SBS96", "SBS192", "SBS1536")
@@ -18,18 +18,22 @@ if (!mutation.type %in% known.sig.types) {
   stop("First argument must be one of ",
           paste(known.sig.types, collapse = ", "))
 }
-
-message(id)
-
-# "Bladder-TCC::SP1003"
-
 ss <- PCAWG7::spectra$PCAWG[[mutation.type]]
-if (!id %in% colnames(ss)) stop(id, " not found")
+
+for (dd in id) {
+  if (!dd %in% colnames(ss)) stop(dd, " not found")
+}
 
 cc <- ss[ , id, drop = FALSE]
 
-id <- gsub("::", "_", id)
-ff <- paste0(mutation.type, "_", id, ".csv")
+message("dim(cc) = ", dim(cc))
+
+message(paste(id, collapse = ", "))
+
+file.id <- gsub("::", "_", id[1])
+
+message("file.id = ", file.id)
+ff <- paste0(mutation.type, "_", file.id, ".csv")
 
 cc <- ICAMS::as.catalog(cc)
 
