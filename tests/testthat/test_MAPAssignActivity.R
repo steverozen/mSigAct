82b4e257095ac9953eb222366fc70bac8ed5b2ef
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-02-25 18:22:07 +0800

    Updated test for MAPAssignActivity()

--- tests/testthat/test_MAPAssignActivity.R
+++ tests/testthat/test_MAPAssignActivity.R
@@ -27,28 +27,24 @@ test_that("MAPAssignActivity for ID Catalog", {
   
   expect_equal(length(retval), 2)
   
-  if (FALSE) {
-    tb1 <- tibble::tibble(sig.id = c("ID1", "ID2", "ID3", "ID5", "ID8",  
-                                     "ID9", "ID10"), 
-                          count = c(ID1 = 60.0957928396838, 
-                                    ID2 = 12.5753318347774,  
-                                    ID3 = 20.5025441148954, 
-                                    ID5 = 74.6080111814531, 
-                                    ID8 = 19.7204896759284,  
-                                    ID9 = 15.131816682705, 
-                                    ID10 = 13.366013670557))
-    expect_equal(retval[[1]]$MAP, tb1)
-    
-    tb2 <- tibble::tibble(sig.id = c("ID1", "ID2", "ID3", "ID4", "ID5",  
-                                     "ID8"), 
-                          count = c(ID1 = 86.4049290774173, 
-                                    ID2 = 5.66462263579074,  
-                                    ID3 = 20.901676243643, 
-                                    ID4 = 96.6166407779654, 
-                                    ID5 = 155.562140415472,  
-                                    ID8 = 47.8499908497115))
-    expect_equal(retval[[2]]$MAP, tb2)
-  }
+  expect_true (all.equal(retval[[1]]$MAP$count, 
+                         c(ID1 = 60.0957928396838, 
+                           ID2 = 12.5753318347774,  
+                           ID3 = 20.5025441148954, 
+                           ID5 = 74.6080111814531, 
+                           ID8 = 19.7204896759284,  
+                           ID9 = 15.131816682705, 
+                           ID10 = 13.366013670557),
+                         tolerance = 1e-3))
+  
+  expect_true(all.equal(retval[[2]]$MAP$count, 
+                        c(ID1 = 86.4049290774173, 
+                          ID2 = 5.66462263579074,  
+                          ID3 = 20.901676243643, 
+                          ID4 = 96.6166407779654, 
+                          ID5 = 155.562140415472,  
+                          ID8 = 47.8499908497115),
+                        tolerance = 1e-3))
  
   unlink(output.dir, recursive = TRUE)
   
