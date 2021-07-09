63c2094e46c4a080ce47b54d1adf55cbf08013e2
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-07-09 18:43:36 +0800

    Added tests for calculating likelihood using multinomial distribution

--- tests/testthat/test_multinomial_likelihood.R
+++ tests/testthat/test_multinomial_likelihood.R
@@ -10,32 +10,33 @@ test_that("Use multinomial distribution to calculate likelihood", {
     PCAWG7::spectra$other.genome$SBS96[, "Skin-Melanoma::ML_31_T_01", drop = FALSE]
   
   # Use negative binomial distribution to calculate log likelihood
-  my.opts1 <- DefaultManyOpts(likelihood.dist = "neg.binom")
   MAP.out1 <- 
     MAPAssignActivity(spectra = skin.spectra,
                       sigs = PCAWG7::signature$genome$SBS96,
                       sigs.presence.prop = prior.prop1,
                       max.level = length(prior.prop1) - 1,
-                      m.opts = my.opts1,
+                      m.opts = DefaultManyOpts(likelihood.dist = "neg.binom"),
                       mc.cores.per.sample = 30,
                       max.subsets = 1e10,
+                      seed = 8386,
                       output.dir = file.path(tempdir(), "skin.mapout1"))
   
   expect_equal(MAP.out1$reconstruction.distances$MAP.distances$cosine,
-               0.9517956)
+               0.9525457, tolerance = 1e-5)
   
-  # Use multinomial distribution to calculate log likelihood
-  my.opts2 <- DefaultManyOpts()
-  my.opts2$trace <- 100
+  # Use multinomial distribution to calculate log likelihood (the default)
   MAP.out2 <- 
     MAPAssignActivity(spectra = skin.spectra,
                       sigs = PCAWG7::signature$genome$SBS96,
                       sigs.presence.prop = prior.prop1,
                       max.level = length(prior.prop1) - 1,
-                      m.opts = my.opts2,
                       mc.cores.per.sample = 30,
-                      max.subsets = 1000000,
+                      max.subsets = 1e10,
+                      seed = 8386,
                       output.dir = file.path(tempdir(), "skin.mapout2"))
-  unlink(file.path(tempdir(), "skin.mapout1"))
+  expect_equal(MAP.out2$reconstruction.distances$MAP.distances$cosine,
+               0.9947313, tolerance = 1e-5)
   
+  unlink(file.path(tempdir(), "skin.mapout1"), recursive = TRUE)
+  unlink(file.path(tempdir(), "skin.mapout2"), recursive = TRUE)
 })
