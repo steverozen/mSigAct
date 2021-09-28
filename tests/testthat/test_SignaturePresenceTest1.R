777151ef86ae6176854418d423e6b39bc91b1d93
jnh01 <44010028+jnh01@users.noreply.github.com>
2021-09-28 15:45:45 +0800

    Create test_SignaturePresenceTest1.R

---
+++ tests/testthat/test_SignaturePresenceTest1.R
@@ -0,0 +1,25 @@
+context("SignaturePresenceTest1")
+
+test_that("Use signature id in SignaturePresenceTest1", {
+  
+  indices <- grep("Liver-HCC", colnames(PCAWG7::spectra$PCAWG$SBS96))
+  spectra <- PCAWG7::spectra$PCAWG$SBS96[, indices[1], drop = FALSE]
+  sigs <- PCAWG7::signature$genome$SBS96
+  sigs.prop <- ExposureProportions(mutation.type = "SBS96",
+                                   cancer.type = "Liver-HCC")
+  sigs.to.test <- sigs[, names(sigs.prop), drop = FALSE]
+  test.out <- SignaturePresenceTest1(spectrum = spectra,
+                                     sigs = sigs.to.test,
+                                     target.sig.index = "SBS22")
+  
+  test.out1 <- SignaturePresenceTest1(spectrum = spectra,
+                                     sigs = sigs.to.test,
+                                     target.sig.index = 13)
+  
+  test.out2 <- 
+    expect_error(SignaturePresenceTest1(spectrum = spectra,
+                                        sigs = sigs.to.test,
+                                        target.sig.index = "SBS10a"))
+  expect_equal(test.out$statistic, 156.1548, tolerance = 1e-5)
+  expect_equal(test.out, test.out1)
+})
