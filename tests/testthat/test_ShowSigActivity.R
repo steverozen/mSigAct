50fe1d9fcd673ff5340c6362b00b86ca63fc4a93
jnh01 <44010028+jnh01@users.noreply.github.com>
2022-05-30 10:13:52 +0800

    Updated subtracted spectrum plot to show the counts to be sum of absolute value of each peak

--- tests/testthat/test_ShowSigActivity.R
+++ tests/testthat/test_ShowSigActivity.R
@@ -50,6 +50,64 @@ test_that("ShowSigActivity for multiple SBS96 catalog", {
   sapply(output.dirs, FUN = unlink, recursive = TRUE)
 })
 
+test_that("ShowSigActivity for only one SBS192 catalog", {
+  spect <- PCAWG7::spectra$PCAWG$SBS192[, 1, drop = FALSE]
+  spect.name <- colnames(spect)
+  index <- grep(spect.name, x = colnames(PCAWG7::exposure$PCAWG$SBS96))
+  exposure <- PCAWG7::exposure$PCAWG$SBS96[, index, drop = FALSE]
+  rownames(exposure) <- cosmicsig::SBS96_ID_to_SBS192_ID(rownames(exposure))
+  sigs <- cosmicsig::COSMIC_v3.2$signature$GRCh37$SBS192
+  exposure2 <- ReassignmentQP(spectra = spect, 
+                              exposure = exposure, 
+                              sigs = sigs)
+  sigs.prop <- ExposureProportions(mutation.type = "SBS192",
+                                   cancer.type = "Biliary-AdenoCA")
+  retval <- AddSigActivity(spect = spect, exposure = exposure2, sigs = sigs,
+                           sigs.presence.prop = sigs.prop)
+  expect_equal(length(retval), 1)
+  
+  output.dirs <- file.path(tempdir(), paste0("test", 1:2))
+  ShowSigActivity(list.of.sig.activity = retval,
+                  output.dir = output.dirs[1],
+                  base.filename = "Biliary-AdenoCA")
+  ShowSigActivity(list.of.sig.activity = retval,
+                  output.dir = output.dirs[2],
+                  base.filename = "Biliary-AdenoCA",
+                  plot.all.samples.in.one.pdf = FALSE,
+                  plot.exposure.proportion = TRUE)
+  
+  sapply(output.dirs, FUN = unlink, recursive = TRUE)
+})
+
+test_that("ShowSigActivity for multiple SBS192 catalog", {
+  spect <- PCAWG7::spectra$PCAWG$SBS192[, 1:2, drop = FALSE]
+  spect.name <- colnames(spect)
+  indices <-
+    sapply(spect.name, FUN = grep, x = colnames(PCAWG7::exposure$PCAWG$SBS96))
+  exposure <- PCAWG7::exposure$PCAWG$SBS96[, indices, drop = FALSE]
+  rownames(exposure) <- cosmicsig::SBS96_ID_to_SBS192_ID(rownames(exposure))
+  sigs <- cosmicsig::COSMIC_v3.2$signature$GRCh37$SBS192
+  exposure2 <- ReassignmentQP(spectra = spect, 
+                              exposure = exposure, 
+                              sigs = sigs)
+  sigs.prop <- ExposureProportions(mutation.type = "SBS192",
+                                   cancer.type = "Biliary-AdenoCA")
+  retval <- AddSigActivity(spect = spect, exposure = exposure2, sigs = sigs,
+                           sigs.presence.prop = sigs.prop)
+  expect_equal(length(retval), 2)
+  
+  output.dirs <- file.path(tempdir(), paste0("test", 1:2))
+  ShowSigActivity(list.of.sig.activity = retval,
+                  output.dir = output.dirs[1],
+                  base.filename = "Biliary-AdenoCA")
+  ShowSigActivity(list.of.sig.activity = retval,
+                  output.dir = output.dirs[2],
+                  base.filename = "Biliary-AdenoCA",
+                  plot.all.samples.in.one.pdf = FALSE,
+                  plot.exposure.proportion = TRUE)
+  sapply(output.dirs, FUN = unlink, recursive = TRUE)
+})
+
 test_that("ShowSigActivity for only one DBS78 catalog", {
   spect <- PCAWG7::spectra$PCAWG$DBS78[, 1, drop = FALSE]
   spect.name <- colnames(spect)
