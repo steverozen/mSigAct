a31d72107132f06749f3e24d91c85c951872d977
jnh01 <44010028+jnh01@users.noreply.github.com>
2023-02-13 17:21:08 +0800

    Investigation of potential error of mSigAct algorithm

---
+++ data-raw/c.r.s_error/c.r.s_error.R
@@ -0,0 +1,27 @@
+
+
+calculate_p_thresh <- function(p_thresh_level, sig) {
+  if (p_thresh_level == "mid") {
+    p_thresh <- 0.05 / ncol(sig)
+  } else if (p_thresh_level == "veryhigh") {
+    p_thresh <- 0.1
+  } else if (p_thresh_level == "low") {
+    p_thresh <- 0.01 / (4 * ncol(sig))
+  } else if (p_thresh_level == "vlow") {
+    p_thresh <- 0.001 / (4 * ncol(sig))
+  } else {
+    stop("unknown p_thresh_level, ", p_thresh_level)
+  }
+  return(p_thresh)
+}
+
+test <-
+  mSigAct::PresenceAssignActivity(spectra = liver_sample_to_test, 
+                                  sigs = liver_sig_non_msi, 
+                                  output.dir = "data-raw/c.r.s_error/test", 
+                                  max.level = ncol(liver_sig_non_msi) - 1, 
+                                  p.thresh = calculate_p_thresh(p_thresh_level = "vlow", sig = liver_sig_non_msi), 
+                                  m.opts = my_opts, 
+                                  num.parallel.samples = 1, 
+                                  mc.cores.per.sample = 100, 
+                                  seed = 145879)
